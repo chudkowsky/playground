1aeadb9f3e940d69bcb9c7030124302eebf7b8f5
Mateusz <mateuszchudy03@gmail.com>
2024-12-04 19:36:32 +0100

    test1

--- cairo-lang-0.13.3/services/everest/definitions/general_config.py
+++
@@ -1,8 +0,0 @@
-import marshmallow_dataclass
-
-from services.config.general_config import GeneralConfigBase
-
-
-@marshmallow_dataclass.dataclass
-class EverestGeneralConfig(GeneralConfigBase):
-    pass
--- cairo-lang-0.13.3/starkware/cairo/lang/create_cairo_lang_package_zip_exe.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import sys
-
-cmd = [
-    sys.executable, "-u", "-s", "-m", 'starkware.cairo.lang.create_cairo_lang_zip'
-] + sys.argv[1:]
-proc = subprocess.run(cmd)
-sys.exit(proc.returncode)
